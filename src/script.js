4b901e58cefeba67306b8d773716c2cde6046fa3
Jânio Alberto da Nóbrega Júnior <87720450+NobreJanio@users.noreply.github.com>
2022-03-16 22:19:37 -0300

    Inicio Array

--- src/script.js
+++ src/script.js
@@ -1,3 +1,13 @@
-var filme1 = "Yesterday"
-var filme2 = "A chegada"
-var filme3 = "Escola do Rock"
+var listaFilmes = ["Yesterday", "A Chagada", "Escola do Rock"];
+
+listaFilmes.push("Harry Potter");
+
+// document.write("<p>" + listaFilmes[0] + "</p>");
+// document.write("<p>" + listaFilmes[1] + "</p>");
+// document.write("<p>" + listaFilmes[2] + "</p>");
+// document.write("<p>" + listaFilmes[3] + "</p>");
+
+for (var indice = 0; indice < 4; indice = indice + 1) {
+    document.write("<p>" + listaFilmes[0] + "</p>")
+
+}
